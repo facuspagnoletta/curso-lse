33ab7e2e4405890b940a2867e4ac2c96c960f2ac
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-05-26 10:08:14 -0300

    .

---
+++ workspace_lpc845/01_blinky/main.c
@@ -0,0 +1,22 @@
+#include "board.h"
+
+#define LED_Blue  GPIO,1,1
+#define delay(t)  for (unsigned int i = 0; i < t; i++);
+
+int main (void) {
+    //Inicializo puerto 1
+    GPIO_PortInit (GPIO,1);
+    
+    //Inicializo el pin 1 del puerto 1 como salida
+    gpio_pin_config_t out_config = {.pinDirection = kGPIO_DigitalOutput, .outputLogic=1 };
+    GPIO_PinInit(GPIO,1,1,&out_config);
+
+    while (1)
+    {
+        GPIO_PinWrite(LED_Blue,GPIO_PinRead(LED_Blue)); //Enciendo el led
+        delay(50000)
+        
+    }
+
+    return 0;
+}
